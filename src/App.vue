3a93ef0df15a9b25287139d1230100be8a389021
MohsenSami <mohsensami88@gmail.com>
2022-09-16 21:35:18 +0430

    spin loadin svg added to btn

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,14 @@
   <div class="flex flex-col items-center mt-24 gap-16">
     <h1 class="text-4xl">نمایش وضعیت آب و هوا</h1>
     <div class="flex w-1/2">
-      <input v-model="city" type="text" class="block focus:outline-0 p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-100 border-l-2 border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white " placeholder="اسم شهر را وارد کنید" >
-      <button @click="getData()" class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-l-lg">ارسال</button>
+      <input v-model="city" type="text" class="flex-1 block focus:outline-0 p-2.5 z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-100 border-l-2 border border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white " placeholder="اسم شهر را وارد کنید" >
+      <button @click="getData()" :class="{'cursor-not-allowed' : loading}" class=" bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-l-lg flex items-center">
+        {{ !loading ? 'ارسال' : 'درحال دریافت اطلاعات' }}
+        <svg v-if="loading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+          <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+      </button>
     </div>
     
       <div class="bg-green-200 text-green-600 text-center w-full p-4" v-if="state.result[0]">
@@ -31,16 +37,15 @@ export default {
   name: 'App',
   setup() {
     const API_KEY = 'ab3d8a663bf1ef42a7f37d1641a6982e'
-
+    const loading = ref(false);
     const city = ref('')
-
     const errors = ref(null)
-
     const state = reactive({
       result : []
     })
 
     const getData = () => {
+      loading.value = true;
       axios.get(`/weather`, {
         params: {
           q: city.value,
@@ -49,12 +54,13 @@ export default {
           lang: 'fa'
         }
       }).then(({ data }) => {
-        errors.value = null
-        console.log(data);
-        state.result.push(data)
-        city.value = null
+        loading.value = false;
+        errors.value = null;
+        state.result.push(data);
+        city.value = null;
       })
       .catch(({ response }) => {
+        loading.value = false;
         if (response.data) {
           state.result = []
           errors.value = response.data.message
@@ -73,7 +79,8 @@ export default {
       getData,
       city,
       state,
-      errors
+      errors,
+      loading
     }
   }
 }
