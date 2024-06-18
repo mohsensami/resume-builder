c7e70c86a15bc4d891c6accca3824384b7501c49
mohsensami <mohsensami18727@gmail.com>
2024-06-18 22:53:28 +0330

    App routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import SignInPage from './auth/sign-in/index.jsx';
+import Home from './home/index.jsx';
+import Dashboard from './dashboard/index.jsx';
+
+const router = createBrowserRouter([
+    {
+        element: <App />,
+        children: [
+            {
+                path: '/',
+                element: <Home />,
+            },
+            {
+                path: '/dashboard',
+                element: <Dashboard />,
+            },
+        ],
+    },
+    {
+        path: '/auth/sign-in',
+        element: <SignInPage />,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
