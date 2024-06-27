0013c6feade4dcbad8a426e8cdc033921f64b11e
mohsensami <mohsensami18727@gmail.com>
2024-06-27 22:06:26 +0330

    experience section form added

--- src/dashboard/resume/components/FormSection.jsx
+++ src/dashboard/resume/components/FormSection.jsx
@@ -3,7 +3,7 @@ import PersonalDetail from './forms/PersonalDetail';
 import { Button } from '@/components/ui/button';
 import { ArrowLeft, ArrowRight, Home, LayoutGrid } from 'lucide-react';
 import Summery from './forms/Summery';
-// import Experience from './forms/Experience';
+import Experience from './forms/Experience';
 // import Education from './forms/Education';
 // import Skills from './forms/Skills';
 import { Link, Navigate, useParams } from 'react-router-dom';
@@ -50,8 +50,7 @@ function FormSection() {
             ) : activeFormIndex == 2 ? (
                 <Summery enabledNext={(v) => setEnableNext(v)} />
             ) : activeFormIndex == 3 ? (
-                // <Experience />
-                <div />
+                <Experience />
             ) : activeFormIndex == 4 ? (
                 // <Education />
                 <div />
